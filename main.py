848e9851b07d9ff1840fd689c269a8f449972aad
Your Name <you@example.com>
2024-11-11 10:20:16 +0300

    Implement three functions, each of which is called when you enter the desired punt

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 print("1. Пункт 1")
 print("2. Пункт 2")
 print("3. Пункт 3")
+
+def option_1():
+    print("Вы выбрали Пункт 1")
+
+def option_2():
+    print("Вы выбрали Пункт 2")
+
+def option_3():
+    print("Вы выбрали Пункт 3")
