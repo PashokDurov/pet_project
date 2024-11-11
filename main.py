9bde2e2fdb4dd01978305bcf7e9cd059aea2363e
Your Name <you@example.com>
2024-11-11 10:34:21 +0300

    Move all functions from main to my_library and import this library into main

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
+from my_library import *
 print("1. Пункт 1")
 print("2. Пункт 2")
 print("3. Пункт 3")
 print("4. Пункт 4")
 
-def option_1():
-    print("Вы выбрали Пункт 1")
 
-def option_2():
-    print("Вы выбрали Пункт 2")
-
-def option_3():
-    print("Вы выбрали Пункт 3")
-
-def option_4():
-    print("Вы выбрали Пункт 4")
---
+++ my_library.py
@@ -0,0 +1,11 @@
+def option_1():
+    print("Вы выбрали Пункт 1")
+
+def option_2():
+    print("Вы выбрали Пункт 2")
+
+def option_3():
+    print("Вы выбрали Пункт 3")
+
+def option_4():
+    print("Вы выбрали Пункт 4")
